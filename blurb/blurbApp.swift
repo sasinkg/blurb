9d8b1400c8f6584832318dc1a3fec3df46f1bdd3
sasinkg <sasingudipati@hotmail.com>
2026-08-26 12:20:06 -0700

    Initial Commit

---
+++ blurb/blurbApp.swift
@@ -0,0 +1,17 @@
+//
+//  blurbApp.swift
+//  blurb
+//
+//  Created by Sasin on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct blurbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blurbTests/blurbTests.swift
@@ -0,0 +1,16 @@
+//
+//  blurbTests.swift
+//  blurbTests
+//
+//  Created by Sasin on 8/26/26.
+//
+
+import Testing
+
+struct blurbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
